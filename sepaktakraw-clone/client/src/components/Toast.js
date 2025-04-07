de0d760d7bdaa0e87d6551e64b06f3b40bd83039
Yash <114616632+yashdahiya0@users.noreply.github.com>
2025-04-07 22:41:12 +0530

    Add sepaktakraw-clone/client/src/components/Toast.js 1744045874170

---
+++ sepaktakraw-clone/client/src/components/Toast.js
@@ -0,0 +1,49 @@
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export const showToast = (message, type = 'default') => {
+  const options = {
+    position: 'top-right',
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: 'colored'
+  };
+
+  switch (type) {
+    case 'success':
+      toast.success(message, options);
+      break;
+    case 'error':
+      toast.error(message, options);
+      break;
+    case 'warning':
+      toast.warning(message, options);
+      break;
+    case 'info':
+      toast.info(message, options);
+      break;
+    default:
+      toast(message, options);
+  }
+};
+
+const Toast = () => (
+  <ToastContainer
+    position="top-right"
+    autoClose={5000}
+    hideProgressBar={false}
+    newestOnTop={false}
+    closeOnClick
+    rtl={false}
+    pauseOnFocusLoss
+    draggable
+    pauseOnHover
+    theme="colored"
+  />
+);
+
+export default Toast;
